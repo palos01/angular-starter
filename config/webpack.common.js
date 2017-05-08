aa86c7f7c05986226426bcb51b68fb2f4e700b44
Pavel Vybiral <vybiral@gmail.com>
2017-05-08 17:27:00 +0200

    Add support for Bootstrap4

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -22,7 +22,7 @@ const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
 const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const ngcWebpack = require('ngc-webpack');
-//const PreloadWebpackPlugin = require('preload-webpack-plugin');
+const ProvidePlugin = require('webpack/lib/ProvidePlugin');
 
 /**
  * Webpack Constants
@@ -63,6 +63,7 @@ module.exports = function (options) {
     entry: {
 
       'polyfills': './src/polyfills.browser.ts',
+      'twbs':      'bootstrap-loader',
       'main':      AOT ? './src/main.browser.aot.ts' :
                   './src/main.browser.ts'
 
@@ -190,7 +191,7 @@ module.exports = function (options) {
           exclude: [helpers.root('src/index.html')]
         },
 
-        /**
+        /*
          * File loader for supporting images, for example, in CSS files.
          */
         {
@@ -203,6 +204,27 @@ module.exports = function (options) {
         {
           test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
           use: 'file-loader'
+        },
+
+        /*
+         * Bootstrap 4 loader
+         */
+        {
+          test: /bootstrap\/dist\/js\/umd\//,
+          use: 'imports-loader?jQuery=jquery'
+        },
+
+        /*
+         * Font loaders, required for font-awesome-sass-loader and bootstrap-loader
+         */
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        },
+
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "file-loader"
         }
 
       ],
@@ -411,6 +433,25 @@ module.exports = function (options) {
          */
         resourceOverride: helpers.root('config/resource-override.js')
       }),
+
+      new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery",
+          "window.jQuery": "jquery",
+          Tether: "tether",
+          "window.Tether": "tether",
+          Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+          Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+          Button: "exports-loader?Button!bootstrap/js/dist/button",
+          Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+          Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+          Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+          Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+          Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+          Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+          Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+          Util: "exports-loader?Util!bootstrap/js/dist/util"
+        })
 
       /**
        * Plugin: InlineManifestWebpackPlugin
